3afb89dbc50fae5189982597794cecb189551a9d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-29 11:39:16 -0300

    Initial commit

---
+++ Assets/Scripts/DialogeManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogeManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogueUI;
+    [SerializeField]TextMeshProUGUI textoDelDialogo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //al entrar activa la UI de diálogo
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            textoDelDialogo.text = "Hola forastero!";
+            dialogueUI.SetActive(true);
+        }
+        
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //al salir desactiva la UI de diálogo
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            dialogueUI.SetActive(false);
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# SistemaDeDialogo-ScriptableObjects
+ 
